1992bfa26aaee3ed844149f4b464adf0310f4aa8
Richard Gerum <richard.gerum+dev@protonmail.com>
2020-06-16 23:21:33 +0200

    store also stories done by anonymous users

--- backend/stories/set_story_done.php
+++ backend/stories/set_story_done.php
@@ -4,10 +4,13 @@ include('../functions_new.php');
 
 echo isset($_GET['id']);
 echo isset($_SESSION["user"]);
-if(isset($_GET['id']) && isset($_SESSION["user"])) {
+if(isset($_GET['id'])) {
     $db = database();
     $story_id = sqlSafeInt($_GET['id']);
-    $user_id = $_SESSION["user"]['id'];
+    if(isset($_SESSION["user"]))
+        $user_id = $_SESSION["user"]['id'];
+    else
+        $user_id = "NULL";
     
     $query = "INSERT INTO story_done (user_id, story_id) VALUES($user_id, $story_id)";
     echo $query;
