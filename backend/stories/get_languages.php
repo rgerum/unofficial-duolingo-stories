03f5cd804c722f4896ce16feb86693e5692060b4
Richard Gerum <richard.gerum+dev@protonmail.com>
2020-06-17 22:30:27 +0200

    display more information in the editor overview: story count per language and story done count per story

--- backend/stories/get_languages.php
+++ backend/stories/get_languages.php
@@ -2,7 +2,11 @@
 session_start();
 include('../functions_new.php');
 
-$query = "SELECT * FROM language";
+$query = "
+SELECT language.id, language.short, language.name, COUNT(story.id) count FROM language
+LEFT JOIN story ON story.lang = language.id
+GROUP BY language.id
+";
 
 queryDatabase($query);
 
--- backend/stories/get_list_editor.php
+++ backend/stories/get_list_editor.php
@@ -7,8 +7,9 @@ if(isset($_GET['lang']) && isset($_GET['lang_base'])) {
     $lang = sqlSafeString($_GET['lang']);
     $lang_base = sqlSafeString($_GET['lang_base']);
     $query = "
-SELECT story.id, story.name, story.xp, name_base, user.username, story.date, story.change_date FROM story 
+SELECT story.id, story.name, story.xp, story.name_base, COUNT(done.id) count, user.username, story.date, story.change_date FROM story
 LEFT JOIN user ON story.author = user.id
+LEFT JOIN story_done done ON story.id = done.story_id
 JOIN language l1 ON story.lang = l1.id AND l1.short = $lang
 JOIN language l2 ON story.lang_base = l2.id AND l2.short = $lang_base
 GROUP BY story.id
