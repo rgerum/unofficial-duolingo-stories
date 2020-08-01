60e004b7bcb6992a3c3722900ada0baeba4d3d02
Richard Gerum <richard.gerum+dev@protonmail.com>
2020-08-01 09:55:31 +0200

    sort course list by count

--- backend/stories/get_courses.php
+++ backend/stories/get_courses.php
@@ -3,11 +3,12 @@ session_start();
 include('../functions_new.php');
 
 $query = "
-SELECT course.id, l1.short AS fromLanguage, l1.name AS fromLanguageName, l2.short AS learningLanguage, l2.name AS learningLanguageName, COUNT(story.id) count, course.public FROM course
+SELECT course.id, l1.short AS fromLanguage, l1.name AS fromLanguageName, l2.short AS learningLanguage, l2.name AS learningLanguageName, COUNT(story.id) count, course.public, course.official FROM course
 LEFT JOIN language l1 ON l1.id = course.fromLanguage
 LEFT JOIN language l2 ON l2.id = course.learningLanguage
 LEFT JOIN story ON (story.lang = course.learningLanguage AND story.lang_base = course.fromLanguage)
-GROUP BY course.id;
+GROUP BY course.id
+ORDER BY COUNT DESC;
 ";
 
 queryDatabase($query);
