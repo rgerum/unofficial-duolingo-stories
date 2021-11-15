5c61e023f4b3e0732a8c6ad982a963bfa0255cb9
rgerum <14153051+rgerum@users.noreply.github.com>
2021-11-14 20:33:29 -0500

    moved to react app structure

---
+++ stories-app/src/App.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import './App.css';
+import {useDataFetcher2, useEventListener} from "./hooks";
+import {getLanguageNames} from "./api_calls";
+import {IndexContent} from "./index_react";
+import {Story} from "./story_react";
+import {Task} from "./task";
+
+
+function App(props) {
+  let urlParams = new URLSearchParams(window.location.search);
+  // activate
+  const [task, setTask] = React.useState(urlParams.get("task") || null);
+  // story
+  const [story, setStory] = React.useState(urlParams.get("story") || null);
+  const [course, setCourse] = React.useState([urlParams.get("lang") || undefined, urlParams.get("lang_base") || undefined]);
+  // language_dataRefetch
+  let [language_data, ] = useDataFetcher2(getLanguageNames, []);
+
+  function changeStory(id, task) {
+    setStory(id);
+    if(id) {
+      window.history.pushState({story: id}, "Story" + id, `?story=${id}`);
+      dispatchEvent(new CustomEvent('progress_changed', {detail: id}));
+    }
+    else if(task) {
+      doSetTask(task);
+    }
+    else {
+      doSetCourse(course);
+    }
+  }
+  function doSetCourse(course) {
+    if(course[0] === undefined)
+      window.history.pushState({course: course}, "Language"+course, ``);
+    else
+      window.history.pushState({course: course}, "Language"+course, `?lang=${course[0]}&lang_base=${course[1]}`);
+    setCourse(course);
+  }
+  function doSetTask(task) {
+    //urlParams.get("username") // username=test7&activation_link=dd2182ff-e8ec-4bc7-822b-4eeb7e624c27
+    window.history.pushState({task: task}, "Language", `?task=${task}&username=${urlParams.get("username")}&activation_link=${urlParams.get("activation_link")}`);
+    setTask(task);
+  }
+
+  useEventListener("popstate", (event) => {
+    if(event.state.story)
+      changeStory(event.state.story)
+    else {
+      setStory(null);
+      if(event.state.course)
+        doSetCourse(event.state.course)
+      else
+        doSetCourse([undefined, undefined])
+    }
+  })
+
+  let [initialized, setInitialized] = React.useState(0);
+  if(!initialized) {
+    let urlParams = new URLSearchParams(window.location.search);
+    setInitialized(1);
+    changeStory(urlParams.get('story'), urlParams.get('task'));
+  }
+  if(task !== null) {
+    return <Task task={task} />
+  }
+  else if(story === null)
+    return <IndexContent language_data={language_data} course={course} setCourse={doSetCourse} onStartStory={changeStory} />
+  return <Story language_data={language_data} story={story} onQuit={()=>{changeStory(null)}} />
+}
+
+export default App;
---
+++ stories-app/src/api_calls.js
@@ -0,0 +1,128 @@
+let backend = "https://carex.uber.space/stories/backend/"
+let backend_stories = backend+"stories/"
+window.backend_stories = backend_stories
+
+export async function getLanguageNames() {
+    /**
+     * Get the language data table.
+     * @type {Response}
+     */
+    let response = await fetch(`${backend_stories}get_languages.php`);
+    let data = await response.json();
+    window.language_data = {};
+    for(let lang of data) {
+        window.language_data[lang.short] = lang;
+    }
+    return window.language_data;
+}
+
+export async function getCourses() {
+    try {
+        let response_courses = await fetch(`${backend_stories}get_courses.php`);
+        let data_courses = await response_courses.json();
+        return data_courses;
+    }
+    catch (e) {
+        return [];
+    }
+}
+
+export async function getPublicCourses() {
+    try {
+        let response_courses = await fetch(`${backend_stories}get_courses.php`);
+        let data_courses = await response_courses.json();
+        let public_courses = [];
+        for(let course of data_courses)
+            if(course.public)
+                public_courses.push(course)
+        return public_courses;
+    }
+    catch (e) {
+        return [];
+    }
+}
+
+export async function getStories(lang, lang_base) {
+    try {
+        let response = await fetch(`https://carex.uber.space/stories/backend/stories/get_list.php?lang=${lang}&lang_base=${lang_base}`);
+        return await response.json();
+    }
+    catch (e) {
+        return [];
+    }
+}
+
+export async function getStoriesSets(lang, lang_base) {
+    try {
+        let response = await fetch(`https://carex.uber.space/stories/backend/stories/get_list.php?lang=${lang}&lang_base=${lang_base}`);
+        let data =  await response.json();
+
+        let set = -1;
+        let sets = [];
+        for(let d of data) {
+            if (set !== d.set_id) {
+                set = d.set_id;
+                sets.push([]);
+            }
+            sets[sets.length - 1].push(d);
+        }
+        return sets;
+    }
+    catch (e) {
+        return [];
+    }
+}
+
+export async function getStoriesEditor(lang, lang_base) {
+    try {
+        let response = await fetch(`https://carex.uber.space/stories/backend/stories/get_list_editor.php?lang=${lang}&lang_base=${lang_base}`);
+        return await response.json();
+    }
+    catch (e) {
+        return undefined;
+    }
+}
+
+export async function setPublic(id, is_public) {
+    return await fetch(backend_stories+"set_story_public.php?id="+id+"&public="+is_public);
+}
+
+export async function getStoryJSON(id) {
+    let response_json = await fetch(`${backend_stories}get_story_json.php?id=${id}`);
+    let story_json;
+    if(response_json.status === 200) {
+        try {
+            story_json = await response_json.json();
+        } catch (e) {
+            story_json = undefined;
+        }
+        if (story_json) {
+            return story_json;
+        }
+    }
+}
+
+export function isCourseValid(data_courses, lang, lang_base) {
+    let valid_course = false;
+    for(let course of data_courses) {
+        if(course.learningLanguage === lang && course.fromLanguage === lang_base) {
+            valid_course = true;
+        }
+    }
+    return valid_course;
+}
+
+export async function getLexicon(lang) {
+    try {
+        let response = await fetch(`https://carex.uber.space/stories/backend/stories/get_lexicon.php?lang=${lang}`);
+        return await response.json();
+    }
+    catch (e) {
+        return {};
+    }
+}
+
+export async function setStoryDone(id) {
+    await fetch(`${backend_stories}set_story_done.php?id=${id}`);
+}
+
