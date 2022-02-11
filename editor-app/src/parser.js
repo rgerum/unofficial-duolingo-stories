1ca6f5456760f4c4a9a04edd8a15a124db265a2a
rgerum <14153051+rgerum@users.noreply.github.com>
2022-02-10 22:26:40 -0500

    syntax parser rewrite

--- editor-app/src/parser.js
+++ editor-app/src/parser.js
@@ -1,16 +1,380 @@
 import {StreamLanguage} from "@codemirror/stream-parser"
 
+const STATE_DEFAULT = "default";
+
+const STATE_DATA_KEY = "heading";
+const STATE_DATA_VALUE = "name";
+
+const STATE_TRANS_EVEN = "propertyName";
+const STATE_TRANS_ODD = "macroName";
+const STATE_TEXT_EVEN = "tagName";
+const STATE_TEXT_ODD = "name";
+
+const STATE_TEXT_HIDE_EVEN = "className";
+const STATE_TEXT_HIDE_ODD = "typeName";
+
+const STATE_TEXT_BUTTON_EVEN = "number";
+const STATE_TEXT_BUTTON_ODD = "labelName";
+const STATE_TEXT_BUTTON_RIGHT_EVEN = "modifier";
+
+const STATE_BLOCK_TYPE = "keyword";
+const STATE_SPEAKER_TYPE = STATE_DATA_KEY;
+
+const STATE_ERROR = "deleted";
+
+const STATE_AUDIO = "color";
+
+function parserTextWithTranslation(stream, state, allow_hide, allow_buttons) {
+    if (allow_hide) {
+        if (stream.eat("[")) {
+            state.bracket = true;
+            return STATE_DEFAULT;
+        }
+        if (stream.eat("]")) {
+            state.bracket = false;
+            return STATE_DEFAULT;
+        }
+    }
+    if (allow_buttons === 2)
+        if (stream.match(/\(\+[^\(\)]*\)/)) {
+            return STATE_TEXT_BUTTON_RIGHT_EVEN;
+        }
+    if (allow_buttons)
+        if (stream.match(/\([^\(\)]*\)/)) {
+            if (state.bracket)
+                return STATE_TEXT_BUTTON_ODD
+            return STATE_TEXT_BUTTON_EVEN
+        }
+
+    if(stream.match(/ *[^ $\]\[]+ */)) {
+        state.odd = !state.odd
+
+        if (state.odd)
+            return STATE_TEXT_EVEN
+        return STATE_TEXT_ODD
+    }
+    stream.skipToEnd();
+    return STATE_ERROR;
+}
+
+
+function parserTranslation(stream, state) {
+    if(stream.match(/ *[^ $\]\[]+ */)) {
+        state.odd = !state.odd
+
+        if (state.odd)
+            return STATE_TRANS_EVEN
+        return STATE_TRANS_ODD
+    }
+    stream.skipToEnd();
+    return STATE_ERROR;
+}
+
+function parserPair(stream, state) {
+    if(state.odd === false) {
+        stream.match(/.*(?=<>)/);
+        state.odd = true;
+        return STATE_TEXT_ODD
+    }
+    else if(stream.eat("<>"))
+        return STATE_DEFAULT
+    else {
+        stream.skipToEnd();
+        return STATE_TRANS_ODD
+    }
+}
+
+function parseBockData(stream, state) {
+    if(stream.match(/[^=]+/)) {
+        if(state.odd) {
+            state.odd = false;
+            return STATE_DATA_VALUE;
+        }
+        return STATE_DATA_KEY;
+    }
+    if(stream.eat("=")) {
+        state.odd = true;
+        return STATE_DEFAULT
+    }
+
+    stream.skipToEnd();
+    return STATE_ERROR;
+}
+
+function parseBockHeader(stream, state) {
+    if(stream.sol()) {
+        if(stream.eat(">") && state.block.line===0) {
+            startLine(state, 1, true, "text", true);
+            return STATE_DEFAULT;
+        }
+        if(stream.eat("~") && state.block.allow_trans) {
+            startLine(state, 1, false, "trans");
+            return STATE_DEFAULT;
+        }
+        if(stream.eat("$") && state.block.allow_audio) {
+            startLine(state); stream.skipToEnd();
+            return STATE_AUDIO;
+        }
+    }
+    if(state.block.line_type === "text")
+        return parserTextWithTranslation(stream, state)
+    if(state.block.line_type === "trans")
+        return parserTranslation(stream, state)
+
+    stream.skipToEnd();
+    return STATE_ERROR;
+}
+
+function parseBockLine(stream, state) {
+    if(stream.sol()) {
+        if(stream.eat(">") && state.block.line===0) {
+            startLine(state, 1, true, "text", true);
+            return STATE_DEFAULT;
+        }
+        if(stream.match(/\S+:/) && state.block.line===0) {
+            startLine(state, 1, true, "text", true);
+            return STATE_SPEAKER_TYPE;
+        }
+        if(stream.eat("~") && state.block.allow_trans) {
+            startLine(state, 1, false, "trans");
+            return STATE_DEFAULT;
+        }
+        if(stream.eat("$") && state.block.allow_audio) {
+            startLine(state); stream.skipToEnd();
+            return STATE_AUDIO;
+        }
+
+        stream.skipToEnd();
+        return STATE_ERROR;
+    }
+    if(state.block.line_type === "text")
+        return parserTextWithTranslation(stream, state)
+    if(state.block.line_type === "trans")
+        return parserTranslation(stream, state)
+
+    stream.skipToEnd();
+    return STATE_ERROR;
+}
+
+function startLine(state, line, allow_trans, line_type, allow_audio) {
+    if(line)
+        state.block.line = line;
+    if(allow_audio === undefined && state.block.allow_trans && state.block.allow_audio)
+        allow_audio = state.block.allow_audio;
+    state.block.allow_audio = allow_audio;
+    state.block.allow_trans = allow_trans;
+    state.odd = false;
+    state.block.line_type = line_type;
+}
+
+function parseBockSelectPhrase(stream, state) {
+    if(stream.sol()) {
+        if(stream.eat(">") && state.block.line===0) {
+            startLine(state, 1, true, "text");
+            return STATE_DEFAULT;
+        }
+        if(stream.eat("~") && state.block.allow_trans) {
+            startLine(state, undefined, false, "trans");
+            return STATE_DEFAULT;
+        }
+        if(stream.match(/\S+:/) && state.block.line===1) {
+            startLine(state, 2, true, "text", true);
+            return STATE_SPEAKER_TYPE;
+        }
+        if(stream.eat("$") && state.block.allow_audio) {
+            startLine(state); stream.skipToEnd();
+            return STATE_AUDIO;
+        }
+
+        if(stream.eat("+") && state.block.line>=2) {
+            startLine(state, 3, false, "text");
+            stream.skipToEnd();
+            return STATE_DEFAULT;
+        }
+        if(stream.eat("-") && state.block.line>=2) {
+            startLine(state, 3, false, "text");
+            stream.skipToEnd();
+            return STATE_DEFAULT;
+        }
+
+        stream.skipToEnd();
+        return STATE_ERROR;
+    }
+    if(state.block.line_type === "text")
+        return parserTextWithTranslation(stream, state, state.block.line === 2)
+    if(state.block.line_type === "trans")
+        return parserTranslation(stream, state)
+
+    stream.skipToEnd();
+    return STATE_ERROR;
+}
+
+function parseBockMultipleChoice(stream, state) {
+    if(stream.sol()) {
+        if(stream.eat(">") && state.block.line===0) {
+            startLine(state, 1, true, "text");
+            return STATE_DEFAULT;
+        }
+        if(stream.eat("~") && state.block.allow_trans) {
+            startLine(state, undefined, false, "trans");
+            return STATE_DEFAULT;
+        }
+        if(stream.eat("+") && state.block.line>=1) {
+            startLine(state, 2, true, "text");
+            return STATE_DEFAULT;
+        }
+        if(stream.eat("-") && state.block.line>=1) {
+            startLine(state, 2, true, "text");
+            return STATE_DEFAULT;
+        }
+
+        stream.skipToEnd();
+        return STATE_ERROR;
+    }
+    if(state.block.line_type === "text")
+        return parserTextWithTranslation(stream, state)
+    if(state.block.line_type === "trans")
+        return parserTranslation(stream, state)
+
+    stream.skipToEnd();
+    return STATE_ERROR;
+}
+
+function parseBockArrange(stream, state) {
+    if(stream.sol()) {
+        if(stream.eat(">") && state.block.line===0) {
+            startLine(state, 1, true, "text");
+            return STATE_DEFAULT;
+        }
+        if(stream.eat("~") && state.block.allow_trans) {
+            startLine(state, undefined, false, "trans");
+            return STATE_DEFAULT;
+        }
+        if(stream.match(/\S+:/) && state.block.line===1) {
+            startLine(state, 2, true, "text", true);
+            return STATE_SPEAKER_TYPE;
+        }
+        if(stream.eat("$") && state.block.allow_audio) {
+            startLine(state); stream.skipToEnd();
+            return STATE_AUDIO;
+        }
+
+        stream.skipToEnd();
+        return STATE_ERROR;
+    }
+    if(state.block.line_type === "text")
+        return parserTextWithTranslation(stream, state, state.block.line === 2, state.block.line === 2)
+    if(state.block.line_type === "trans")
+        return parserTranslation(stream, state)
+
+    stream.skipToEnd();
+    return STATE_ERROR;
+}
+
+function parseBockPointToPhrase(stream, state) {
+    if(stream.sol()) {
+        if(stream.eat(">") && state.block.line===0) {
+            startLine(state, 1, true, "text");
+            return STATE_DEFAULT;
+        }
+        if(stream.eat("~") && state.block.allow_trans) {
+            startLine(state, undefined, false, "trans");
+            return STATE_DEFAULT;
+        }
+        if(stream.match(/\S+:/) && state.block.line===1) {
+            startLine(state, 2, true, "text", true);
+            return STATE_SPEAKER_TYPE;
+        }
+        if(stream.eat("$") && state.block.allow_audio) {
+            startLine(state); stream.skipToEnd();
+            return STATE_AUDIO;
+        }
+
+        stream.skipToEnd();
+        return STATE_ERROR;
+    }
+    if(state.block.line_type === "text")
+        return parserTextWithTranslation(stream, state, state.block.line === 2, (state.block.line === 2)*2)
+    if(state.block.line_type === "trans")
+        return parserTranslation(stream, state)
+
+    stream.skipToEnd();
+    return STATE_ERROR;
+}
+
+
+function parseBockMatch(stream, state) {
+    if(stream.sol()) {
+        if(stream.eat(">") && state.block.line===0) {
+            startLine(state, 1, true, "text");
+            return STATE_DEFAULT;
+        }
+        if(stream.eat("~") && state.block.allow_trans) {
+            startLine(state, undefined, false, "trans");
+            return STATE_DEFAULT;
+        }
+        if(stream.eat("-") && state.block.line===1) {
+            startLine(state, 1, false, "pair");
+            return STATE_DEFAULT;
+        }
+
+        stream.skipToEnd();
+        return STATE_ERROR;
+    }
+    if(state.block.line_type === "text")
+        return parserTextWithTranslation(stream, state)
+    if(state.block.line_type === "trans")
+        return parserTranslation(stream, state)
+    if(state.block.line_type === "pair")
+        return parserPair(stream, state)
+
+    stream.skipToEnd();
+    return STATE_ERROR;
+}
+
+const BLOCK_FUNCS = {
+    "DATA": parseBockData,
+    "HEADER": parseBockHeader,
+    "LINE": parseBockLine,
+    "MULTIPLE_CHOICE": parseBockMultipleChoice,
+    "SELECT_PHRASE": parseBockSelectPhrase,
+    "ARRANGE": parseBockArrange,
+    "MATCH": parseBockMatch,
+    "POINT_TO_PHRASE": parseBockPointToPhrase,
+}
+
+function parseBlockDef(stream, state) {
+    if(stream.eat("]")) {
+        state.func = BLOCK_FUNCS[state.block.name];
+        return STATE_DEFAULT;
+    }
+    state.block = {name: stream.match(/[^\]]+/)[0], line: 0};
+    return STATE_BLOCK_TYPE;
+}
+
 function parserWithMetadata(stream, state) {
+    if(stream.sol()) {
+        stream.match(/\s*/);
+        if(stream.eat("[")) {
+            state.func = parseBlockDef;
+            return STATE_DEFAULT;
+        }
+    }
+    if(state.func)
+        return state.func(stream, state);
+
+    stream.skipToEnd();
+    return STATE_ERROR;
     // new element
     if(state.pos === "default" && stream.eat("[")) {
         state.pos = "type";
-        return "default"
+        return STATE_DEFAULT
     }
     // inside new element
     if(state.pos === "type") {
         if(stream.eat("]")) {
             state.pos = "default";
-            return "default"
+            return STATE_DEFAULT
         }
         let type = stream.match(/[^\]]+/)[0];
         state.type = type;
@@ -21,42 +385,26 @@ function parserWithMetadata(stream, state) {
         if(stream.match(/[^=]+/)) {
             if(state.odd) {
                 state.odd = false;
-                return "name";
+                return STATE_TEXT_ODD;
             }
             return "heading";
         }
         if(stream.eat("=")) {
             state.odd = true;
-            return "default"
+            return STATE_DEFAULT
         }
     }
     if(state.pos === "speech") {
         if(stream.match(/ +/)) {
             if(state.bracket)
-                return "className"
-            return "default";
+                return STATE_TEXT_HIDE_EVEN
+            return STATE_DEFAULT;
         }
         if(!state.translate) {
-            if (state.type === "ARRANGE" || state.type === "CONTINUATION" || "SELECT_PHRASE") {
-                if (stream.eat("[")) {
-                    state.bracket = true;
-                    return "default";
-                }
-                if (stream.eat("]")) {
-                    state.bracket = false;
-                    return "default";
-                }
-            }
-            if (state.type === "POINT_TO_PHRASE")
-                if (stream.match(/\(\+[^\(\)]*\)/)) {
-                    return "modifier"
-                }
-            if (state.type === "POINT_TO_PHRASE" || state.type === "ARRANGE")
-                if (stream.match(/\([^\(\)]*\)/)) {
-                    if (state.bracket)
-                        return "labelName"
-                    return "number"
-                }
+            parserTextWithTranslation(stream, state,
+                state.type === "ARRANGE" || state.type === "CONTINUATION" || state.type === "SELECT_PHRASE",
+                (state.type === "POINT_TO_PHRASE" || state.type === "ARRANGE") + (state.type === "POINT_TO_PHRASE")
+                )
         }
         if(stream.eol()) {
             stream.skipToEnd()
@@ -77,13 +425,13 @@ function parserWithMetadata(stream, state) {
             state.odd = !state.odd
             if(state.translate === true) {
                 if (state.odd)
-                    return "propertyName"
-                return "macroName"
+                    return STATE_TRANS_EVEN
+                return STATE_TRANS_ODD
             }
             if(state.bracket) {
                 if (state.odd)
-                    return "className"
-                return "typeName"
+                    return STATE_TEXT_HIDE_EVEN
+                return STATE_TEXT_HIDE_ODD
             }
             if(state.paranthese) {
                 if (state.odd)
@@ -91,8 +439,8 @@ function parserWithMetadata(stream, state) {
                 return "modifier"
             }
             if (state.odd)
-                return "tagName"
-            return "name"
+                return STATE_TEXT_EVEN
+            return STATE_TEXT_ODD
         }
         else {
             stream.skipToEnd()
@@ -107,20 +455,20 @@ function parserWithMetadata(stream, state) {
         if(state.odd === false) {
             if(stream.match(/[^<>]*/)) {
                 state.odd = undefined
-                return "tagName"
+                return STATE_TEXT_EVEN
             }
         }
         else if(state.odd === undefined) {
             if(stream.match(/ *<> */)) {
                 state.odd = true
-                return "default"
+                return STATE_DEFAULT
             }
         }
         else {
             stream.skipToEnd()
             state.pos = "default"
             state.odd = false;
-            return "propertyName";
+            return STATE_TRANS_EVEN;
         }
     }
     if(state.type === "LINE" || state.type === "HEADER"
@@ -156,7 +504,7 @@ function parserWithMetadata(stream, state) {
                 state.pos = "match_part";
                 state.odd = false;
                 state.translate = false;
-                return "default"
+                return STATE_DEFAULT
             }
         }
         if(state.type === "MULTIPLE_CHOICE"
@@ -168,17 +516,18 @@ function parserWithMetadata(stream, state) {
                 state.odd = false;
                 state.translate = false;
                 state.bracket = false;
-                return "default"
+                return STATE_DEFAULT
             }
             if (stream.eat("-")) {
                 state.pos = "speech";
                 state.odd = false;
                 state.translate = false;
                 state.bracket = false;
-                return "default"
+                return STATE_DEFAULT
             }
         }
     }
+    state.eol();
     state.odd = false;
     state.bracket = false;
     return "deleted"
