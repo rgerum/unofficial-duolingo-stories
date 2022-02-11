2953cc98f9291f45c305ef53f825cd8f1025e5a4
rgerum <14153051+rgerum@users.noreply.github.com>
2022-02-10 22:26:03 -0500

    fixed email sending

--- backend/user/user.php
+++ backend/user/user.php
@@ -17,6 +17,21 @@ require_once 'lib_swift/swift_required.php';
 include("hash_functions.php");
 
 
+function uuidv4($data = null) {
+    // Generate 16 bytes (128 bits) of random data or use the data passed into the function.
+    $data = $data ?? random_bytes(16);
+    assert(strlen($data) == 16);
+
+    // Set version to 0100
+    $data[6] = chr(ord($data[6]) & 0x0f | 0x40);
+    // Set bits 6-7 to 10
+    $data[8] = chr(ord($data[8]) & 0x3f | 0x80);
+
+    // Output the 36 character UUID.
+    return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
+}
+
+
 function checkUsername($username, $existing) {
     global $db;
     $result = mysqli_query($db,"SELECT id, email FROM user WHERE LOWER(username) = LOWER('$username')");
@@ -165,7 +180,7 @@ else if($action == "send") {
     if($result) {
         send_email(
         "[Unofficial Duolingo Stories] Reset Password $username",
-        array("stories@carex.uberspace.de" => "Unofficial Duolingo Stories"),
+        "stories@carex.uberspace.de",
         $email,
         "Hey $username,<br/>
         You or someone else has requested a new password for 'Unofficial Duolingo Stories'.<br/>
