b05e29f73d289ee527997b35730006faa30f9a03
rgerum <14153051+rgerum@users.noreply.github.com>
2022-03-19 14:54:13 -0400

    fixed setting story_done

--- stories-app/src/story_react.jsx
+++ stories-app/src/story_react.jsx
@@ -683,7 +683,7 @@ export class Story extends React.Component {
     }
 
     finish() {
-        setStoryDone(this.props.story);
+        setStoryDone(this.props.story_id);
         this.props.onQuit();
     }
 
