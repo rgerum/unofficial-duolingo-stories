aa84a83586e94bd8bd1f30aee3954aa78b641457
IFrzzl <83833754+IFrzzl@users.noreply.github.com>
2022-06-06 23:52:17 +0100

    Dropdown custom flag align (#114)

--- packages/stories-app/src/react/flag.jsx
+++ packages/stories-app/src/react/flag.jsx
@@ -6,7 +6,8 @@ export function Flag(props) {
      * A big flag button
      * @type {{flag_file: string, flag: number}}
      */
-    return <div className={"flag "+props.className}
+    return <div className={"flag "+props.className + (props.flag_file ? " custom-flag":"")}
                 style={props.flag_file ? {backgroundImage: `url(https://duostories.org/stories/flags/${props.flag_file})`} : {backgroundPosition: `0 ${props.flag}px`}}
+                
     />
 }
