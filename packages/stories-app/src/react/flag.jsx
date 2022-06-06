e2d68ebed900c15d652f53c3cf327ae583b7637e
IFrzzl <83833754+IFrzzl@users.noreply.github.com>
2022-06-06 20:19:23 +0100

    Update flag.jsx

--- packages/stories-app/src/react/flag.jsx
+++ packages/stories-app/src/react/flag.jsx
@@ -6,7 +6,8 @@ export function Flag(props) {
      * A big flag button
      * @type {{flag_file: string, flag: number}}
      */
-    return <div className={"flag "+props.className}
+    return <div className={"flag custom-flag "+props.className}
                 style={props.flag_file ? {backgroundImage: `url(https://duostories.org/stories/flags/${props.flag_file})`} : {backgroundPosition: `0 ${props.flag}px`}}
+                
     />
 }
