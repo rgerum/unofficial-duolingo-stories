911d35825148f8b6683dff3ea4c55aff96a00ae7
rgerum <14153051+rgerum@users.noreply.github.com>
2022-06-09 13:19:20 -0400

    fixed course order to reflect current story count

--- backend/stories/get_courses.php
+++ backend/stories/get_courses.php
@@ -9,7 +9,7 @@ SELECT course.id, course.name,
  COUNT(story.id) count, course.public, course.official FROM course
 LEFT JOIN language l1 ON l1.id = course.fromLanguage
 LEFT JOIN language l2 ON l2.id = course.learningLanguage
-LEFT JOIN story ON (story.lang = course.learningLanguage AND story.lang_base = course.fromLanguage)
+LEFT JOIN story ON (course.id = story.course_id)
 GROUP BY course.id
 ORDER BY COUNT DESC;
 ";
