2fd3f5f3b56083c759b2a8e054e17b3c823218ca
rgerum <14153051+rgerum@users.noreply.github.com>
2022-07-08 10:14:14 -0400

    fixed small glitch for base language flags

--- packages/editor-app/src/react/flag.jsx
+++ packages/editor-app/src/react/flag.jsx
@@ -6,7 +6,10 @@ export function Flag(props) {
      * A big flag button
      * @type {{flag_file: string, flag: number}}
      */
+    let factor = 1.0;
+    if(props.className === "flag_sub")
+        factor = 0.9;
     return <div className={"flag "+props.className}
-                style={props.flag_file ? {backgroundImage: `url(https://duostories.org/stories/flags/${props.flag_file})`} : {backgroundPosition: `0 ${props.flag}px`}}
+                style={props.flag_file ? {backgroundImage: `url(https://duostories.org/stories/flags/${props.flag_file})`} : {backgroundPosition: `0 ${props.flag*factor}px`}}
     />
 }
