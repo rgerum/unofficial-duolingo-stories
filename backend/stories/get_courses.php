3a70ecc41b9bbb1b9ff6a54092b86e0f61878bd5
rgerum <14153051+rgerum@users.noreply.github.com>
2022-07-26 08:40:08 -0400

    order courses in overview alphabetically (#128)

--- backend/stories/get_courses.php
+++ backend/stories/get_courses.php
@@ -3,7 +3,7 @@ session_start();
 include('../functions_new.php');
 
 $query = "
-SELECT course.id, course.name,
+SELECT course.id,  COALESCE(NULLIF(course.name, ''), l2.name) as name,
  l1.short AS fromLanguage, l1.name AS fromLanguageName, l1.flag_file AS fromLanguageFlagFile, l1.flag AS fromLanguageFlag,
  l2.short AS learningLanguage, l2.name AS learningLanguageName, l2.flag_file AS learningLanguageFlagFile, l2.flag AS learningLanguageFlag,
  COUNT(story.id) count, course.public, course.official FROM course
@@ -11,7 +11,7 @@ LEFT JOIN language l1 ON l1.id = course.fromLanguage
 LEFT JOIN language l2 ON l2.id = course.learningLanguage
 LEFT JOIN story ON (course.id = story.course_id)
 GROUP BY course.id
-ORDER BY COUNT DESC;
+ORDER BY name;
 ";
 
 queryDatabase($query);
