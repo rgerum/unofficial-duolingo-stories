d764470c5e976e81da20d8c3fb3d80f60b1b6c43
rgerum <14153051+rgerum@users.noreply.github.com>
2022-08-30 19:03:14 -0400

    added conlang field in get_courses.php query

--- backend/stories/get_courses.php
+++ backend/stories/get_courses.php
@@ -6,7 +6,7 @@ $query = "
 SELECT course.id,  COALESCE(NULLIF(course.name, ''), l2.name) as name,
  l1.short AS fromLanguage, l1.name AS fromLanguageName, l1.flag_file AS fromLanguageFlagFile, l1.flag AS fromLanguageFlag,
  l2.short AS learningLanguage, l2.name AS learningLanguageName, l2.flag_file AS learningLanguageFlagFile, l2.flag AS learningLanguageFlag,
- COUNT(story.id) count, course.public, course.official FROM course
+ COUNT(story.id) count, course.public, course.official, course.conlang FROM course
 LEFT JOIN language l1 ON l1.id = course.fromLanguage
 LEFT JOIN language l2 ON l2.id = course.learningLanguage
 LEFT JOIN story ON (course.id = story.course_id)
