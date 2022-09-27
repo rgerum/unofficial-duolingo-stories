9b2b51c4c53f24124fd348f26ee27f018832bc94
rgerum <14153051+rgerum@users.noreply.github.com>
2022-09-27 19:59:07 -0400

    added compression to express app

--- backend_node/app.js
+++ backend_node/app.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const session = require('express-session')
+const compression = require('compression')
 
 const app = express()
+app.use(compression())
+
 const port = 3001
 const path = '/stories/backend_node'
 
