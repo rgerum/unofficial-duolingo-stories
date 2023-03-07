2d3adec25c031d70d264c4e4b940923a282d60a9
rgerum <14153051+rgerum@users.noreply.github.com>
2023-03-06 22:16:02 -0500

    added custom signin page

--- next/next-all/pages/auth/signin.jsx
+++ next/next-all/pages/auth/signin.jsx
@@ -104,9 +104,6 @@ export async function getServerSideProps(context) {
         return { redirect: { destination: "/" } };
     }
 
-    return {
-        props: { providers: {"email":{"id":"email","name":"Email","type":"email","signinUrl":"http://localhost:3000/api/auth/signin/email","callbackUrl":"http://localhost:3000/api/auth/callback/email"},"facebook":{"id":"facebook","name":"Facebook","type":"oauth","signinUrl":"http://localhost:3000/api/auth/signin/facebook","callbackUrl":"http://localhost:3000/api/auth/callback/facebook"},"github":{"id":"github","name":"GitHub","type":"oauth","signinUrl":"http://localhost:3000/api/auth/signin/github","callbackUrl":"http://localhost:3000/api/auth/callback/github"},"discord":{"id":"discord","name":"Discord","type":"oauth","signinUrl":"http://localhost:3000/api/auth/signin/discord","callbackUrl":"http://localhost:3000/api/auth/callback/discord"},"google":{"id":"google","name":"Google","type":"oauth","signinUrl":"http://localhost:3000/api/auth/signin/google","callbackUrl":"http://localhost:3000/api/auth/callback/google"},"credentials":{"id":"credentials","name":"Credentials","type":"credentials","signinUrl":"http://localhost:3000/api/auth/signin/credentials","callbackUrl":"http://localhost:3000/api/auth/callback/credentials"}} },
-    }
     const providers = await getProviders();
 
     return {
