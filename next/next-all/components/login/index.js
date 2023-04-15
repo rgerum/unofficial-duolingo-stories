b1f972bf7fc5c4c2f3dba6e3cbb6222b3619d78c
rgerum <14153051+rgerum@users.noreply.github.com>
2023-04-15 12:59:46 -0400

    fixed login/index.js

--- next/next-all/components/login/index.js
+++ next/next-all/components/login/index.js
@@ -1,3 +1,3 @@
-export {LoggedInButton} from "./loggedinbutton";
+export LoggedInButton from "./loggedinbutton";
 export {Login, LoginDialog} from "./login_dialog"
 export {UserActivationOrReset} from "./user_activation_or_reset";
