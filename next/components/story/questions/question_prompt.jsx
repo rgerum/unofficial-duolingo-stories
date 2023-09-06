8bb996cc06fd8d46253c2f3ef44f7f6dd8c44678
Richard Gerum <14153051+rgerum@users.noreply.github.com>
2023-09-06 18:44:43 -0400

    changed match question prompt to div from span to fix text direction for rtl

--- next/components/story/questions/question_prompt.jsx
+++ next/components/story/questions/question_prompt.jsx
@@ -5,7 +5,7 @@ import HintLineContent from "../text_lines/line_hints";
 
 export default function QuestionPrompt({question, lang}) {
     if(typeof question === "string")
-        return <span className={styles.question+" "+lang}>{question}</span>
+        return <div className={styles.question+" "+lang}>{question}</div>
     return <div className={styles.question+" "+lang}>
         <HintLineContent content={question} />
     </div>
