ac3ae429dc200cf57db85f39fa68ede8714557ee
Richard Gerum <14153051+rgerum@users.noreply.github.com>
2023-10-26 23:33:04 -0400

    added manifest.json

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -4,6 +4,11 @@ import "styles/global.css";
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#1cb0f6" />
+        <title>Duostories</title>
+      </head>
       <body>{children}</body>
     </html>
   );
