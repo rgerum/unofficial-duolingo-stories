cec999fa743b4fee7cf1a10cdedf93782a1e95c1
Richard Gerum <14153051+rgerum@users.noreply.github.com>
2023-11-14 00:10:47 -0500

    added darklight.js as inline script

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,5 +1,6 @@
 import "@fontsource/nunito";
 import "styles/global.css";
+import Script from "next/script";
 
 export default function RootLayout({ children }) {
   return (
@@ -8,7 +9,34 @@ export default function RootLayout({ children }) {
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#1cb0f6" />
         <title>Duostories</title>
-        <script src="/darklight.js"></script>
+        <Script id="darklight">{`function get_current_theme() {
+  // it's currently saved in the document?
+  if (document.body.dataset.theme) {
+    return document.body.dataset.theme;
+  }
+  // it has been previously saved in the window?
+  if (
+    window.localStorage.getItem("theme") !== undefined &&
+    window.localStorage.getItem("theme") !== "undefined"
+  ) {
+    return window.localStorage.getItem("theme");
+  }
+  // or the user has a preference?
+  if (window.matchMedia("(prefers-color-scheme: dark)").matches) return "dark";
+  return "light";
+}
+
+console.log("activeTheme...");
+function load() {
+  let activeTheme = get_current_theme();
+      console.log("activeTheme", activeTheme);
+  document.body.dataset.theme = activeTheme;
+  window.localStorage.setItem("theme", activeTheme);
+}
+load()
+
+document.addEventListener("DOMContentLoaded", load);
+`}</Script>
       </head>
       <body>{children}</body>
     </html>
