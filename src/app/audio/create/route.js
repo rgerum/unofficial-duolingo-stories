4aaca85312fd8bd6b050bf3cce82032ed059c9fb
Richard Gerum <14153051+rgerum@users.noreply.github.com>
2023-11-20 19:26:10 -0500

    fixed error with azure TTS

--- src/app/audio/create/route.js
+++ src/app/audio/create/route.js
@@ -30,49 +30,45 @@ async function exists(filename) {
 }
 
 export async function POST(req) {
-  try {
-    const token = await getToken({ req });
+  const token = await getToken({ req });
 
-    if (!token?.role)
-      return new Response("You need to be a registered contributor.", {
-        status: 401,
-      });
+  if (!token?.role)
+    return new Response("You need to be a registered contributor.", {
+      status: 401,
+    });
 
-    let data = await req.json();
-    let id = parseInt(data.id);
-    let speaker = data.speaker;
-    let text = data.text;
-    let filename = undefined;
-    let file;
-    if (id !== 0) {
-      filename = `../audio/${id}`;
-      try {
-        await mkdir(filename);
-      } catch (e) {}
-      while (true) {
-        file = uuid().split("-")[0] + ".mp3";
-        filename += "/" + file;
-        if (!(await exists(filename))) break;
-      }
+  let data = await req.json();
+  let id = parseInt(data.id);
+  let speaker = data.speaker;
+  let text = data.text;
+  let filename = undefined;
+  let file;
+  if (id !== 0) {
+    filename = `../audio/${id}`;
+    try {
+      await mkdir(filename);
+    } catch (e) {}
+    while (true) {
+      file = uuid().split("-")[0] + ".mp3";
+      filename += "/" + file;
+      if (!(await exists(filename))) break;
     }
-
-    let answer;
-    for (let engine of audio_engines) {
-      if (await engine.isValidVoice(speaker)) {
-        answer = await engine.synthesizeSpeech(filename, speaker, text);
-        answer.engine = engine.name;
-        break;
-      }
-    }
-    if (id !== 0) {
-      answer.output_file = `${id}/` + file;
-    }
-
-    if (answer === undefined)
-      return new Response("Error not found.", { status: 404 });
-
-    return NextResponse.json(answer);
-  } catch (err) {
-    return new Response(err.message, { status: 500 });
   }
+
+  let answer;
+  for (let engine of audio_engines) {
+    if (await engine.isValidVoice(speaker)) {
+      answer = await engine.synthesizeSpeech(filename, speaker, text);
+      answer.engine = engine.name;
+      break;
+    }
+  }
+  if (id !== 0) {
+    answer.output_file = `${id}/` + file;
+  }
+
+  if (answer === undefined)
+    return new Response("Error not found.", { status: 404 });
+
+  return NextResponse.json(answer);
 }
