760fba3c98e2f1fcc1a99c4a27424bcd6b8df83e
Richard Gerum <14153051+rgerum@users.noreply.github.com>
2023-12-06 23:31:40 -0500

    added /learn page as start_url for the PWA

---
+++ src/app/(stories)/learn/page.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { getServerSession } from "next-auth/next";
+import { redirect } from "next/navigation";
+import { authOptions } from "app/api/auth/[...nextauth]/route";
+import { query_one_obj } from "lib/db";
+import Welcome from "./welcome";
+
+export const metadata = {
+  title: "Duostories FAQ",
+  description: "Information about the duostories project.",
+  alternates: {
+    canonical: "https://duostories.org/faq",
+  },
+};
+
+async function get_last_course(username) {
+  return await query_one_obj(
+    `SELECT c.short
+FROM course c
+JOIN story s ON s.course_id = c.id
+JOIN story_done sd ON sd.story_id = s.id
+JOIN user u ON sd.user_id = u.id
+WHERE u.username = ?
+ORDER BY sd.time DESC
+LIMIT 1;`,
+    [username],
+  );
+}
+
+export default async function Page() {
+  const session = await getServerSession(authOptions);
+
+  if (session?.user) {
+    let last_course = await get_last_course(session?.user?.name);
+    console.log(last_course, session?.user?.name, session.user);
+    redirect("/" + last_course.short);
+  }
+
+  return <Welcome />;
+}
