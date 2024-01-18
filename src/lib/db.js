190e340b93c7ffebbb02935403e6d46cdd5bb8af
Richard Gerum <14153051+rgerum@users.noreply.github.com>
2024-01-18 14:50:55 -0500

    db track queries

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,9 +1,29 @@
 import postgres from "postgres";
+import { track } from "@vercel/analytics";
+
+const cyrb53 = (str, seed = 0) => {
+  let h1 = 0xdeadbeef ^ seed,
+    h2 = 0x41c6ce57 ^ seed;
+  for (let i = 0, ch; i < str.length; i++) {
+    ch = str.charCodeAt(i);
+    h1 = Math.imul(h1 ^ ch, 2654435761);
+    h2 = Math.imul(h2 ^ ch, 1597334677);
+  }
+  h1 = Math.imul(h1 ^ (h1 >>> 16), 2246822507);
+  h1 ^= Math.imul(h2 ^ (h2 >>> 13), 3266489909);
+  h2 = Math.imul(h2 ^ (h2 >>> 16), 2246822507);
+  h2 ^= Math.imul(h1 ^ (h1 >>> 13), 3266489909);
+
+  return 4294967296 * (2097151 & h2) + (h1 >>> 0);
+};
 
 // will use psql environment variables
 export const sql = postgres(process.env.POSTGRES_URL, {
   /* options */
   //debug: console.log,
+  debug: (...args) => {
+    track("query", { query: cyrb53(args[1]) });
+  },
   ssl: "require",
 });
 
