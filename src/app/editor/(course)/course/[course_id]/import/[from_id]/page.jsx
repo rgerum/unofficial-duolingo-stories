6e1bf03a078dbb1d997b2264218162bfa7b5b55f
Richard Gerum <14153051+rgerum@users.noreply.github.com>
2024-01-22 15:01:54 -0500

    added import from selector

--- src/app/editor/(course)/course/[course_id]/import/[from_id]/page.jsx
+++ src/app/editor/(course)/course/[course_id]/import/[from_id]/page.jsx
@@ -1,9 +1,15 @@
 import {
   get_course_import,
   get_course_data,
+  get_course_list_data,
+  get_language_list_data,
 } from "../../../../db_get_course_editor";
 import ImportList from "./import_list";
 import { notFound } from "next/navigation";
+import Link from "next/link";
+import { DoubleFlag } from "../../../../../../../components/layout/flag";
+import React from "react";
+import styles from "./import_list.module.css";
 
 export async function generateMetadata({ params }) {
   const course = await get_course_data(params.course_id);
@@ -19,9 +25,11 @@ export async function generateMetadata({ params }) {
 }
 
 export default async function Page({ params }) {
-  let from = params.from_id;
-  let course = await get_course_data(params.course_id);
-  let course_from = await get_course_data(params.from_id);
+  const from = params.from_id;
+  const languages = await get_language_list_data();
+  const course = await get_course_data(params.course_id);
+  const courses = await get_course_list_data();
+  const course_from = await get_course_data(params.from_id);
 
   let imports = await get_course_import({
     from_id: course_from.id,
@@ -32,10 +40,54 @@ export default async function Page({ params }) {
     imports = [];
   }
 
+  let course_data = [];
+  for (let course of courses) {
+    if (course.short === "es-en") course_data.push(course);
+  }
+  for (let course of courses) {
+    if (course.short === "en-es-o") course_data.push(course);
+  }
+  for (let course of courses) {
+    if (
+      course.official &&
+      course.short !== "es-en" &&
+      course.short !== "en-es-o"
+    )
+      course_data.push(course);
+  }
+
   // Render data...
   return (
     <>
-      <ImportList course={course} imports={imports} import_id={from} />
+      <div className={styles.lang_selector}>
+        {course_data.map((c, i) => (
+          <Link
+            key={i}
+            href={`/editor/course/${course.short}/import/${c.short}`}
+          >
+            <span>
+              <span>
+                <DoubleFlag
+                  width={40}
+                  lang1={languages[c.learning_language]}
+                  lang2={languages[c.from_language]}
+                  className={styles.flag}
+                />
+              </span>
+              <span>
+                {languages[c.from_language].short}-
+                {languages[c.learning_language].short}
+              </span>
+            </span>
+          </Link>
+        ))}
+      </div>
+      <ImportList
+        course={course}
+        course_from={course_from}
+        imports={imports}
+        import_id={from}
+      />
     </>
   );
 }
