2506b31c23b886fcf3c5064dc043e6a10dc6830a
Richard Gerum <14153051+rgerum@users.noreply.github.com>
2024-01-22 17:14:26 -0500

    moved darklight.js to separate file

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -13,38 +13,11 @@ export default function RootLayout({ children }) {
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#1cb0f6" />
         <title>Duostories</title>
-        <Script id="darklight">{`function get_current_theme() {
-  // it's currently saved in the document?
-  if (document.body.dataset.theme) {
-    return document.body.dataset.theme;
-  }
-  // it has been previously saved in the window?
-  if (
-    window.localStorage.getItem("theme") !== undefined &&
-    window.localStorage.getItem("theme") !== "undefined"
-  ) {
-    return window.localStorage.getItem("theme");
-  }
-  // or the user has a preference?
-  if (window.matchMedia("(prefers-color-scheme: dark)").matches) return "dark";
-  return "light";
-}
-
-console.log("activeTheme...");
-function load() {
-  let activeTheme = get_current_theme();
-      console.log("activeTheme", activeTheme);
-  document.body.dataset.theme = activeTheme;
-  window.localStorage.setItem("theme", activeTheme);
-}
-load()
-
-document.addEventListener("DOMContentLoaded", load);
-`}</Script>
+        <Script src="/darklight.js"></Script>
       </head>
       <body className={nunito.className}>
         {children}
-        {!process.env.NO_TRACK_QUERY ? <Analytics /> : null}
+        <Analytics />
       </body>
     </html>
   );
