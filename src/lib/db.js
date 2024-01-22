35a112f8f2f556e97fdd106f7add0f2f43ac233b
Richard Gerum <14153051+rgerum@users.noreply.github.com>
2024-01-22 15:58:58 -0500

    db.js do no track when env variable is set

--- src/lib/db.js
+++ src/lib/db.js
@@ -33,7 +33,8 @@ export const sql = postgres(process.env.POSTGRES_URL, {
       }
       console.log("query", { query: hash, sql: args[1] });
     } else {
-      track("query", { query: cyrb53(args[1]) });
+      if (!process.env.NO_TRACK_QUERY)
+        track("query", { query: cyrb53(args[1]) });
     }
   },
   ssl: "require",
