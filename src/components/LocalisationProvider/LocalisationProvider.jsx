92b4c8478147133f331101f15814a28cc3fdea52
Richard Gerum <14153051+rgerum@users.noreply.github.com>
2024-04-14 14:22:47 -0400

    fixed LocalisationProvider.jsx

--- src/components/LocalisationProvider/LocalisationProvider.jsx
+++ src/components/LocalisationProvider/LocalisationProvider.jsx
@@ -1,12 +1,14 @@
+"use server";
 import React from "react";
 import { get_localisation_dict } from "../../lib/get_localisation";
+import { LocalisationProviderInner } from "./LocalisationProviderContext";
 
 function LocalisationProvider({ lang, children }) {
   const data = React.useMemo(() => get_localisation_dict(lang), [lang]);
   return (
-    <localisationContext.Provider value={data}>
+    <LocalisationProviderInner data={data}>
       {children}
-    </localisationContext.Provider>
+    </LocalisationProviderInner>
   );
 }
 
