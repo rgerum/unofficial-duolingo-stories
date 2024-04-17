7e0b2e755654933922c9dcb1d7ebc0a2d86d94ad
Richard Gerum <14153051+rgerum@users.noreply.github.com>
2024-04-17 09:13:58 -0400

    fixes to the fonts

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -5,11 +5,14 @@ import { Analytics } from "@vercel/analytics/react";
 import StyledComponentsRegistry from "lib/registry";
 
 // If loading a variable font, you don't need to specify the font weight
-const nunito = Nunito({ subsets: ["latin-ext", "cyrillic-ext", "vietnamese"] });
+const nunito = Nunito({
+  subsets: ["latin-ext", "cyrillic-ext", "vietnamese"],
+  variable: "--font-nunito",
+});
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className={nunito.className}>
+    <html lang="en" className={nunito.variable}>
       <head>
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#1cb0f6" />
