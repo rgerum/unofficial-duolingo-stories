6b955b1be590233053d4c8922b1c6c7a390e3385
rgerum <14153051+rgerum@users.noreply.github.com>
2025-01-27 20:19:41 -0500

    password reset use user_id not name and add more error message

--- src/app/(stories)/auth/reset_pw/[name]/[hash]/page.jsx
+++ src/app/(stories)/auth/reset_pw/[name]/[hash]/page.jsx
@@ -3,6 +3,12 @@ import ResetPassword from "./reset_pw";
 import { phpbb_hash } from "@/lib/auth/hash_functions2";
 import { sql } from "@/lib/db";
 
+function isValidUUIDv4(uuid) {
+  const uuidV4Regex =
+    /^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
+  return uuidV4Regex.test(uuid);
+}
+
 async function changePasswordAction(password, user_id) {
   let password_hashed = await phpbb_hash(password);
 
@@ -11,10 +17,15 @@ async function changePasswordAction(password, user_id) {
 }
 
 async function check_link(name, hash) {
-  let result = await sql`
-    SELECT id, email FROM "users" WHERE LOWER(name) = LOWER(${name})`;
+  let user_id = parseInt(name);
+  let result = !isNaN(user_id)
+    ? await sql`SELECT id, email FROM "users" WHERE id = ${user_id}`
+    : await sql`SELECT id, email FROM "users" WHERE name = ${name}`;
   if (result.length === 0) {
-    return undefined;
+    return { error: { title: `User '${name}' does not exist.` } };
+  }
+  if (!isValidUUIDv4(hash)) {
+    return { error: { title: `Link '${hash}' has invalid shape.` } };
   }
   // Format the date and time
   const formattedDate = new Date()
@@ -25,17 +36,35 @@ async function check_link(name, hash) {
   let result2 =
     await sql`SELECT id FROM verification_token WHERE token = ${hash} AND identifier = ${result[0].email} AND expires > ${formattedDate} LIMIT 1`;
   if (result2.length === 0) {
-    return undefined;
+    let result3 =
+      await sql`SELECT id,expires FROM verification_token WHERE token = ${hash} AND identifier = ${result[0].email} LIMIT 1`;
+    if (result3 && result3.length > 0) {
+      return {
+        error: {
+          text: `expired ${result3[0].expires}`,
+          title: "Expired link.",
+        },
+      };
+    }
+    return { error: { title: "Invalid Link", text: "" } };
   }
-  return result[0].id;
+  return { data: result[0].id };
 }
 
 export default async function Page({ params }) {
-  let user_id = await check_link(params.name, params.hash);
+  let { data: user_id, error } = await check_link(params.name, params.hash);
+  if (error) {
+    return (
+      <div id="login_dialog">
+        <p id="status">{error.title}</p>
+        <p>{error.text}</p>
+      </div>
+    );
+  }
   if (!user_id) {
     return (
       <div id="login_dialog">
-        <p id="status">Invalid or expired link.</p>
+        <p id="status">Invalid link.</p>
       </div>
     );
   }
