3c15bad66a2e510ac50ae6478b3f404ac42e2092
Richard Gerum <14153051+rgerum@users.noreply.github.com>
2026-07-08 14:31:41 -0400

    Add mobile deep links (#567)

--- next.config.js
+++ next.config.js
@@ -14,6 +14,28 @@ module.exports = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  async headers() {
+    return [
+      {
+        source: "/.well-known/apple-app-site-association",
+        headers: [
+          {
+            key: "Content-Type",
+            value: "application/json",
+          },
+        ],
+      },
+      {
+        source: "/.well-known/assetlinks.json",
+        headers: [
+          {
+            key: "Content-Type",
+            value: "application/json",
+          },
+        ],
+      },
+    ];
+  },
   // PostHog reverse proxy to avoid ad blockers
   async rewrites() {
     return [
