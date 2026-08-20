2163d1f0a1af9cda172268586174f73ddcf83c86
rgerum <14153051+rgerum@users.noreply.github.com>
2026-08-20 17:19:27 -0400

    Balance mobile editor gutter spacing

--- src/app/editor/story/[story]/v2/editor_v2.tsx
+++ src/app/editor/story/[story]/v2/editor_v2.tsx
@@ -63,14 +63,15 @@ const mobileEditorPresentation = [
       fontSize: "16px",
     },
     ".cm-line": {
-      padding: "0 2px",
+      padding: "0 2px 0 4px",
     },
     ".cm-lineNumbers": {
       fontSize: "12px",
+      lineHeight: "22.4px",
     },
     ".cm-lineNumbers .cm-gutterElement": {
       minWidth: "0",
-      padding: "0 0 0 2px",
+      padding: "0 3px 0 2px",
     },
     ".cm-foldGutter": {
       display: "none !important",
